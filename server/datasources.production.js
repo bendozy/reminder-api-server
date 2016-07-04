6c1fa808141c6918f277a25af7a3353a9f19cab1
Chidozie Ijeomah <bendozy@gmail.com>
2016-07-04 14:27:13 +0100

    Modify heroku postgres

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,12 +1,11 @@
 module.exports = {
   default: {
     connector: process.env.DB_TYPE,
-    hostname: process.env.OPENSHIFT_POSTGRESQL_DB_HOST,
-    port: process.env.OPENSHIFT_POSTGRESQL_DB_PORT,
-    user: process.env.OPENSHIFT_POSTGRESQL_DB_USERNAME,
-    url: process.env.OPENSHIFT_POSTGRESQL_DB_URL,
-    password: process.env.OPENSHIFT_POSTGRESQL_DB_PASSWORD,
+    hostname: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    url: process.env.DB_URL,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME
   }
 };
-console.log(process.env.PENSHIFT_POSTGRESQL_DB_URL);
