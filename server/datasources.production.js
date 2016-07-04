6ed3837a9bb71ff07f2b53d7046f3724c2291105
Chidozie Ijeomah <bendozy@gmail.com>
2016-07-04 13:48:35 +0100

    Configure openshit password

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,11 +1,12 @@
 module.exports = {
   default: {
     connector: process.env.DB_TYPE,
-    hostname: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    url: process.env.DB_URL,
-    password: process.env.DB_PASS,
+    hostname: process.env.OPENSHIFT_POSTGRESQL_DB_HOST,
+    port: process.env.OPENSHIFT_POSTGRESQL_DB_PORT,
+    user: process.env.OPENSHIFT_POSTGRESQL_DB_USERNAME,
+    url: process.env.OPENSHIFT_POSTGRESQL_DB_URL,
+    password: process.env.OPENSHIFT_POSTGRESQL_DB_PASSWORD,
     database: process.env.DB_NAME
   }
 };
+console.log(process.env.PENSHIFT_POSTGRESQL_DB_URL);
