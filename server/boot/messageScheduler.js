86ca5812173c090d96af1d774cfc897344bda05d
Chidozie Ijeomah <bendozy@gmail.com>
2016-07-14 22:38:29 +0100

    test again i

--- server/boot/messageScheduler.js
+++ server/boot/messageScheduler.js
@@ -8,7 +8,7 @@ var dataSource = app.dataSources.db;
 
 module.exports = function(app, cb) {
   var Contact = app.models.Contact;
-  var runTime = '00 28 22 * * 0-6';
+  var runTime = '00 44 22 * * 0-6';
   var today = moment();
   var month = '';
   if(today.month() <9){
